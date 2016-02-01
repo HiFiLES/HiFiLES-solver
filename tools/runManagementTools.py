f264b044e1b066757bb941daf6e5a7c54cb8eaae
Manuel <mlopez14@stanford.edu>
2016-01-31 20:55:26 -0800

    management of runs generates all the necessary folders, files, and records

--- tools/runManagementTools.py
+++ tools/runManagementTools.py
@@ -5,6 +5,7 @@ Tools for running HiFiLES cases automatically
 '''
 
 import os
+import time # to pause runs while processors become available
 
 # Importing modules from 'tools' folder
 from inputFileTools import createInputFile # used to create input files
@@ -25,14 +26,19 @@ def setupRunFolder(curFolder, execName, referenceFolder):
 # Creates a folder with name caseName containing:
 # HiFiLES executable
 # pre-processed geometry matrices (.HiFiLES.bin files) from a reference folder
+# meshes present in referenceFolder
 
     # Copy HiFiLES executable to current folder
     if os.system('cp ' + execName + ' ' + curFolder) != 0:
         raise (execName + ' does not exist')
     
     # Copy binary matrix files for this case from the reference folder
-    if os.system('cp ' + os.path.join(referenceFolder, '*.HiFiLES.bin' + ' ' + curFolder)) != 0:
+    if os.system('cp ' + os.path.join(referenceFolder, '*.HiFiLES.bin') + ' ' + curFolder) != 0:
         raise ('Binary matrix files could not be copied from ' + referenceFolder + ' to ' + curFolder)
+    
+    # Copy meshes from reference folder
+    os.system('cp ' + os.path.join(referenceFolder, '*.msh') + ' ' + curFolder)
+    os.system('cp ' + os.path.join(referenceFolder, '*.neu') + ' ' + curFolder)
     
 def assembleRuns(runs):
 # Creates an array of dictionaries that can be readily used to create input files with the createInputFile function
@@ -44,7 +50,9 @@ def assembleRuns(runs):
     """
     
     # Get all keys and values for the dictionary
-    keys = [item for singleRun in runs for item in singleRun[0]]
+    keys = [item \
+            for singleRun in runs \
+            for item in singleRun[0]]
     values = [singleRun[1] for singleRun in runs]
         
     
@@ -55,7 +63,10 @@ def assembleRuns(runs):
     varList = [run[0] for run in runs]
     # Stores the index of varList where the key is found
     # keyLocation[i] = j means that keys[i] is in runs[j][0]
-    keyLocation = [index for key in keys for (index,vars) in enumerate(varList) if key in vars]
+    keyLocation = [index \
+                   for key in keys \
+                   for (index,vars) in enumerate(varList) \
+                   if key in vars]
 
     # Assemble the cases
     runOptions = []
@@ -68,64 +79,79 @@ def assembleRuns(runs):
 
 
 def main():
-    options = dict()
-    options['viscous'] = 0
-    options['dt'] = 1e-5
-    options['order'] = 2
-    options['plot_freq'] = 0
-    options['Mach_free_stream'] = 0.1
-    options['Mach_c_ic'] = options['Mach_free_stream']
-    options['Re_free_stream'] = 200
-    options['Re_c_ic'] = options['Re_free_stream']
+    '''
+    Run a series of cases with different input files
+    '''
+    # Specify number of processors to use per simulation
+    numNodesPerCase = 2
     
+    # Specify prefix of folder where all cases will be stored
+    casePrefix = 'newRuns'
+    
+    # Specify paths to folders and files
     os.environ['HIFILES_HOME'] = '/home/mlopez14/HiFiLES-solver'
-    
-    caseName = 'new'
     homeFolder = os.environ['HIFILES_HOME']
     referenceFolder = os.path.join(homeFolder, 'bin', 'sense')
-    
     referenceInputFile = os.path.join(homeFolder, 'testcases', 'navier-stokes', 'cylinder', 'input_cylinder_visc')
-    
-
-    caseName = 'new'
-    newFolder = os.path.join(homeFolder, 'runs', caseName)
-    
-    os.system('mkdir -p ' + newFolder)
-    
-    newInputFile = os.path.join(newFolder, 'input_file_' + caseName)
-    
     execName = os.path.join(homeFolder, 'bin', 'HiFiLES')
     
-    mfileName = os.path.join(newFolder, 'mfile')
     
-#     freeNodes = getFreeNodes(50)
-    freeNodes = ['compute-0-0', 'compute-0-1']
+    # Specify where summary of cases will be written
+    caseFolder = os.path.join(homeFolder, 'runs', casePrefix)
+    caseFileName = os.path.join(caseFolder, casePrefix + '_case_file')
     
-#     printMachineFile(mfileName, freeNodes, 2)
-#     
-#     createInputFile(referenceInputFile, newInputFile, options)
-#     
-#     setupRunFolder(newFolder, execName, referenceFolder)
+    # Write configuration for the runs in the case file
+    caseFile = open(caseFileName, 'a')
+    try:
+        caseFile.write('homeFolder=' + homeFolder + '\n')
+        caseFile.write('referenceFolder=' + referenceFolder + '\n')
+        caseFile.write('referenceInputFile=' + referenceInputFile + '\n')
+        caseFile.write('execName=' + execName + '\n')
+    finally:
+        caseFile.close()
 
-
-
-    runs = []
     # Create list of variable ranges desired
-    runs.append( (['viscous'], [0]) )
+    runs = []
+    runs.append( (['viscous'], [1]) )
     runs.append( (['dt'], [1e-5]) )
-    runs.append( (['order'], [2]) )
-    runs.append( (['plot_freq'], [0]) )
-    runs.append( (['Mach_free_stream', 'Mach_c_ic'], [0.1, 0.2]) )
-    runs.append( (['Re_free_stream', 'Re_free_stream'], [200, 300]) )
+    runs.append( (['order'], [3]) )
+    runs.append( (['plot_freq'], [1e8]) )
+    runs.append( (['Mach_free_stream', 'Mach_c_ic'], [0.21, 0.2]) )
+    runs.append( (['Re_free_stream', 'Re_c_ic'], [20, 21]) )
 
     runOptions = assembleRuns(runs)
     
     # Generate all input files necessary
     for (index, options) in enumerate(runOptions):
-        newInputFile = os.path.join(newFolder, 'input_file_' + caseName + '_' + str(index))
-        createInputFile(referenceInputFile, newInputFile, options)
+        
+        # Pause runs while processors become free
+        while True:
+        # Gather free nodes
+            freeNodes = ['compute-0-0', 'compute-0-1']
+            # freeNodes = getFreeNodes(50)
+        
+            # Check if the number of free nodes is greater than the number of requested nodes
+            if len(freeNodes) < numNodesPerCase:
+                time.sleep(5)
+            else:
+                caseFile = open(caseFileName, 'a')
+                try:
+                    caseFile.write(casePrefix + str(index) + ': ' + str(options) + '\n')
+                finally:
+                    caseFile.close()
+                break
+
+        newFolder = os.path.join(caseFolder, str(index))
     
-    print runOptions
+        os.system('mkdir -p ' + newFolder)
+        
+        newInputFile = os.path.join(newFolder, 'input_file' )
+        createInputFile(referenceInputFile, newInputFile, options)
+        
+        mfileName = os.path.join(newFolder, 'mfile')
+        printMachineFile(mfileName, freeNodes, 2)
+        
+        setupRunFolder(newFolder, execName, referenceFolder)
 
 
 if __name__ == "__main__": 
